b38ea7077d9959c11082d11bd1873d0fd62de4fa
DanoRiv <danio260496@gmail.com>
2025-03-17 21:49:03 -0500

    Add category application layer

--- src/main/java/com/pragma/home_service/category/application/mappers/CategoryDtoMapper.java
+++ src/main/java/com/pragma/home_service/category/application/mappers/CategoryDtoMapper.java
@@ -3,9 +3,11 @@ package com.pragma.home_service.category.application.mappers;
 import com.pragma.home_service.category.application.dto.request.SaveCategoryRequest;
 import com.pragma.home_service.category.application.dto.response.CategoryResponse;
 import com.pragma.home_service.category.domain.model.CategoryModel;
+import org.mapstruct.Mapper;
 
 import java.util.List;
 
+@Mapper(componentModel = "spring")
 public interface CategoryDtoMapper {
     CategoryModel toModel(SaveCategoryRequest request);
     List<CategoryResponse> toResponse(List<CategoryModel> categories);
