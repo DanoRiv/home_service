97008f8a01e72250668ae0fecbf75a6f43a4da43
DanoRiv <danio260496@gmail.com>
2025-03-19 17:18:16 -0500

    Add ignore to id on mapper

--- src/main/java/com/pragma/home_service/category/application/mappers/CategoryDtoMapper.java
+++ src/main/java/com/pragma/home_service/category/application/mappers/CategoryDtoMapper.java
@@ -4,10 +4,12 @@ import com.pragma.home_service.category.application.dto.request.SaveCategoryRequ
 import com.pragma.home_service.category.application.dto.response.CategoryResponse;
 import com.pragma.home_service.category.domain.model.CategoryModel;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",
+unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CategoryDtoMapper {
     CategoryModel toModel(SaveCategoryRequest request);
     List<CategoryResponse> toResponse(List<CategoryModel> categories);
