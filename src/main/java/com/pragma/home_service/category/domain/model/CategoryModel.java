af0acbade6bce1499032cc0c2189d7daec0c4adf
DanoRiv <danio260496@gmail.com>
2025-03-19 17:12:46 -0500

    Fix category model validations

--- src/main/java/com/pragma/home_service/category/domain/model/CategoryModel.java
+++ src/main/java/com/pragma/home_service/category/domain/model/CategoryModel.java
@@ -4,16 +4,16 @@ import com.pragma.home_service.category.domain.exception.DescriptionLengthExceed
 import com.pragma.home_service.category.domain.exception.NameLengthExceededException;
 
 import static com.pragma.home_service.category.domain.utils.constants.DomainConstants.*;
-import static java.util.Objects.requireNonNull;
+
 public class CategoryModel {
     private Long id;
     private String name;
     private String description;
 
     public CategoryModel(Long id, String name, String description) {
-        this.id = id;
-        this.name = validateName(name);
-        this.description = description;
+        setId(id);
+        setName(name);
+        setDescription(description);
     }
 
     public Long getId() {
@@ -41,12 +41,14 @@ public class CategoryModel {
     }
 
     private String validateName(String name){
+        if(name == null) throw new NullPointerException(FIELD_NAME_NULL_MESSAGE);
         if(name.length() > MAX_NAME_LENGTH) throw new NameLengthExceededException();
-        return requireNonNull(name, FIELD_NAME_NULL_MESSAGE);
+        return name;
     }
 
     private String validateDescription(String description){
+        if(description == null) throw new NullPointerException(FIELD_DESCRIPTION_NULL_MESSAGE);
         if(description.length() > MAX_DESCRIPTION_LENGTH) throw new DescriptionLengthExceededException();
-        return requireNonNull(description, FIELD_DESCRIPTION_NULL_MESSAGE);
+        return description;
     }
 }
